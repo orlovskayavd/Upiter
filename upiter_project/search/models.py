6e11dfcfe5d0388c9176695723f43903d1e4092e
Валентина Орловская <orlovskayavd@gmail.com>
2024-06-06 18:24:00 +0300

    Added new database files and initial migration scripts

--- upiter_project/search/models.py
+++ upiter_project/search/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+class Kazakhstan(models.Model):
+    Trade_name_rus = models.CharField(max_length=256)
+    #Registrator_tran = models.CharField(max_length=256)
+    #Registrator_country = models.CharField(max_length=256)
+    #Producer_tran = models.CharField(max_length=256)
+    #Producer_country = models.CharField(max_length=256)
+    #Dosage_form_full_name = models.CharField(max_length=256)
+    #Dose = models.CharField(max_length=256)
+    #Sc_name = models.CharField(max_length=256)
+    #Recipe_status = models.CharField(max_length=256)
+    As_name_rus = models.CharField(max_length=256)
+    num = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.Trade_name_rus
+    
+    
+class Kyrgyzstan(models.Model):
+    Trade_name_rus = models.CharField(max_length=256)
+    #Registrator_tran = models.CharField(max_length=256)
+    #Registrator_country = models.CharField(max_length=256)
+    #Producer_tran = models.CharField(max_length=256)
+    #Producer_country = models.CharField(max_length=256)
+    #Dosage_form_full_name = models.CharField(max_length=256)
+    #Dose = models.CharField(max_length=256)
+    #Sc_name = models.CharField(max_length=256)
+    #Recipe_status = models.CharField(max_length=256)
+    As_name_rus = models.CharField(max_length=256)
+    num = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.Trade_name_rus
+    
+    
+class Armenia(models.Model):
+    Trade_name_rus = models.CharField(max_length=256)
+    #Registrator_tran = models.CharField(max_length=256)
+    #Registrator_country = models.CharField(max_length=256)
+    #Producer_tran = models.CharField(max_length=256, default='')
+    #Producer_country = models.CharField(max_length=256)
+    #Dosage_form_full_name = models.CharField(max_length=256)
+    #Dose = models.CharField(max_length=256)
+    #Sc_name = models.CharField(max_length=256)
+    #Recipe_status = models.CharField(max_length=256)
+    As_name_rus = models.CharField(max_length=256)
+    num = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.Trade_name_rus
+
+
+class GRLS(models.Model):
+    Trade_name_rus = models.CharField(max_length=256)
+    #Registrator_tran = models.CharField(max_length=256)
+    #Registrator_country = models.CharField(max_length=256) ##############arm krg
+    #Producer_tran = models.CharField(max_length=256) ##########grls
+    #Producer_country = models.CharField(max_length=256)
+    #Dosage_form_full_name = models.CharField(max_length=256)
+    #Dose = models.CharField(max_length=256)
+    #Sc_name = models.CharField(max_length=256) ###############kz
+    #Recipe_status = models.CharField(max_length=256)
+    As_name_rus = models.CharField(max_length=256)
+    num = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.Trade_name_rus
+    
+    
